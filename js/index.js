a8f95cbc2098568742e2c72998b25d7cca9a3680
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-04-22 11:24:16 -0500

    Semana 11

--- js/index.js
+++ js/index.js
@@ -5,65 +5,25 @@ const regBtn = document.getElementById('regBtn');
 const database = firebase.database();
 
 
-const obtainData = ()=>{
-    console.log('ObtainData');
-
+const registrar = ()=>{
     let nombre = nombreET.value;
     let cedula = cedulaET.value;
 
-    database.ref('users/'+cedula).get().then( 
-        (data)=>{
-        console.log('Response');
-        let user = data.val();
-        console.log(user);
-        if(user !== null){
-            alert('Ese dato ya existe');    
-
-        }else{
-            alert('Registrado con exito');    
-            let obj = {
-                nombre:nombre, 
-                cedula:cedula
-            };
-            database.ref('users/'+cedula).set(obj);
-
-        } 
-
-    });
-
-
-
-
-    
-
-
+    let obj = {
+        nombre:nombre, 
+        cedula:cedula
+    };
+    database.ref('users/'+cedula).set(obj);
 }
 
+regBtn.addEventListener('click', registrar);
 
-const votar = ()=>{
 
-    let nombre = nombreET.value;
 
-    database.ref('users/'+nombre).on('value', (data)=>{
-
-        let num = data.numChildren();    
-        console.log(num);
-    
-        data.forEach(
-            (user)=>{
-                console.log(user.val());
-            }
-        );
-        num
+database.ref('users').on('value',  (data)=>{
+    data.forEach(user => {
+        console.log(user.val());
     });
-
-}
-
-
-
-regBtn.addEventListener('click', obtainData);
-
-
-
+});
 
 
